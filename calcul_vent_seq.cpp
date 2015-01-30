983bffac075ca63b1d8064d596cebe7f5295b293
Dranen <mathieu.debonydelavergne@epfl.ch>
2015-01-30 17:47:49 +0100

    Ajout de Calcul_vent_seq et correction d'erreur de compilations

---
+++ calcul_vent_seq.cpp
@@ -0,0 +1,5 @@
+#include "calcul_vent_seq.h"
+
+Calcul_vent_seq::Calcul_vent_seq()
+{
+}
---
+++ calcul_vent_seq.h
@@ -0,0 +1,10 @@
+#ifndef CALCUL_VENT_SEQ_H
+#define CALCUL_VENT_SEQ_H
+
+class Calcul_vent_seq
+{
+public:
+    Calcul_vent_seq();
+};
+
+#endif // CALCUL_VENT_SEQ_H
